aa66f834ebb59e4a44d062deccf507e4fe844e11
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-03-03 14:17:11 +0530

    Set up and Data insertin n hibernate

---
+++ HibernetFirstProj/src/main/java/com/rvr/app/LaunchFirstApp.java
@@ -0,0 +1,43 @@
+package com.rvr.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.rvr.model.Student;
+
+public class LaunchFirstApp {
+
+    public static void main(String[] args) {
+
+        Configuration config = new Configuration();
+        config.configure("hibernate.cfg.xml");
+
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+
+            Student student = new Student();
+            student.setsid(1);
+            student.setsname("kalyan");
+            student.setscity("ongole");
+
+            session.save(student);   // VERY IMPORTANT
+
+            transaction.commit();
+            System.out.println("Student saved successfully!");
+
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+}
