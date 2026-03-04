8e6bf79200e04ec7945d6bc607d9df0f7bf59e47
KalyanRam Ayinabathina <kalyan799ram@gmail.com>
2026-03-04 21:06:43 +0530

    added project builded using hibernate configuration without .cfg.xml file

---
+++ HibernateConfigurationWithoutXml/src/main/java/com/rvr/model/Student1.java
@@ -0,0 +1,48 @@
+package com.rvr.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="Student_table")
+public class Student1 {
+@Id
+@Column(name="student_id")
+private int sid;
+@Column(name="Student_name")
+private String sname;
+@Column(name="student_city")
+private String scity;
+public int getsid() {
+	return sid;
+}
+public Student1() {
+	System.out.println("student constructor");
+}
+public void setsid(int sid) {
+	this.sid = sid;
+}
+public String getsname() {
+	return sname;
+}
+public void setsname(String sname) {
+	this.sname = sname;
+}
+public String getscity() {
+	return scity;
+}
+public void setscity(String scity) {
+	this.scity = scity;
+}
+@Override
+public String toString() {
+	return "Student [sid=" + sid + ", sname=" + sname + ", scity=" + scity + "]";
+}
+
+
+
+}
+
